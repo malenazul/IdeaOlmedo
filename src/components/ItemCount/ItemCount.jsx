f62fe06b996608756903cbb66b610986767c54ed
malenazul <54649999+malenazul@users.noreply.github.com>
2022-04-26 17:21:09 -0300

    Componentes 2 Corrección 2

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -2,29 +2,48 @@
 /* eslint-disable no-const-assign */
 import React from "react";
 import "./ItemCount.css";
+import { useState } from "react";
 
-const ItemCount = ({ stock, initial, onAdd, onSubstract, disponibles }) => {
+const ItemCount = ({ stock, initial, onAdd }) => {
+
+ const [contador, setContador] = useState(initial);
+  function onSumar(){
+    setContador(contador + 1);
+
+  }
+  function onRestar(){
+    setContador(contador - 1);
+  }
   return (
+    <div className="row">
     <div className="row" style={{ float: "center", margin: "2%" }}>
       <div style={{ float: "left", display: "flex" }}>
         <button
           className="btn btn-outline-warning"
-          onClick={initial > 0 ? onSubstract : ""}
-          style={{ width: "10%" }}
+          onClick={contador > initial ? onRestar : console.log("Ya no puede restar")}
+          style={{ width: "4rem" }}
         >
           - 1
         </button>
-        <h5 className="txt" style={{ width: "30%" }}>
-          Pasajeros : {initial}
+        <h5 className="txt" style={{ width: "11rem" }}>
+          Pasajeros : {contador}
         </h5>
         <button
           className="btn btn-outline-warning"
-          onClick={initial < stock ? onAdd : ""}
-          style={{ width: "10%" }}
+          onClick={contador < stock ? onSumar : console.log("Ya no puede sumar")}
+          style={{ width: "4rem" }}
         >
           + 1
         </button>
       </div>
+    </div>
+    <div className="row boton" style={{ float: "center"}}>
+    <button
+          className="btn btn-outline-info"
+          onClick={onAdd}
+          style={{ width: "18.9rem" }}
+        >Agregar al carrito</button>
+    </div>
     </div>
   );
 };
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -7,22 +7,24 @@ import berlin from "./berlin.jpeg";
 import paris from "./paris.jpg";
 import "./ItemListContainer.css";
 import ItemCount from "../ItemCount/ItemCount";
-import { useState } from "react";
+
 const ItemListContainer = ({ greeting }) => {
-  const [contador, setContador] = useState(0);
+  
   let stock = 10;
+  let initial = 0;
 
   return (
     // eslint-disable-next-line jsx-a11y/anchor-is-valid
 
     <div className="container" id="listContainer">
       <h3 className="txt1">{greeting}</h3>
+      <div className="row" style={{ float: "center" }}>
       <ItemCount
         stock={stock}
-        initial={contador}
-        onAdd={() => setContador(contador + 1)}
-        onSubstract={() => setContador(contador - 1)}
+        initial={initial}
+        onAdd={() => console.log("Se agregaron los elementos")}
       ></ItemCount>
+      </div>
       <div className="row" style={{ float: "center" }}>
         <Card
           imagen={roma}
