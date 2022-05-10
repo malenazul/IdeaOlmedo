27510e56bfb9b4fd96ad435b167a88315a1b0d77
malenazul <54649999+malenazul@users.noreply.github.com>
2022-05-09 21:32:28 -0300

    Detalle

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,23 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./Item.css";
-import ItemDetailContainer from "../ItemDetailContainer/ItemDetailContainer";
 import { NavLink } from "react-router-dom";
 
-
-
-
 const Item = ({ imagen, texto, nombre, precio, id }) => {
-  const [visible, setVisible] = useState(false);
-
-  function onDetail() {
-    visible === false ? setVisible(true) : setVisible(false);
-
-  }
-
-  useEffect(() => {
-    return () => {};
-  }, []);
-
 
   return (
     <div
@@ -32,20 +17,12 @@ const Item = ({ imagen, texto, nombre, precio, id }) => {
       <h3 className="txt2t">{nombre}</h3>
       <img src={imagen} className="card-img-top" alt={nombre} />
       <div className="card-body">
-        <button
-          className="btn btn-outline-info"
-          style={{ width: "10rem" }}
-
-        >
+        <button className="btn btn-outline-info" style={{ width: "10rem" }}>
           <NavLink className="nav-link" to={`/Item/${id}`}>
             Mas Detalles
           </NavLink>
         </button>
       </div>
-
-
-        
-      
     </div>
   );
 };
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -9,17 +9,16 @@ const ItemList = ({ ciudades }) => {
         {ciudades.length > 0 ? (
           ciudades.map((ciudad, index) => (
             <React.Fragment key={index}>
-            <Item
-
-              imagen={ciudad.pictureUrl}
-              texto={ciudad.description}
-              nombre={ciudad.title}
-              precio={ciudad.price}
-              id={ciudad.id}
-            ></Item>
-          </React.Fragment>
-        ))
-      ) : (
+              <Item
+                imagen={ciudad.pictureUrl}
+                texto={ciudad.description}
+                nombre={ciudad.title}
+                precio={ciudad.price}
+                id={ciudad.id}
+              ></Item>
+            </React.Fragment>
+          ))
+        ) : (
           <div className="row col-12">
             <h4 className="txt2l">Cargando...</h4>
           </div>
